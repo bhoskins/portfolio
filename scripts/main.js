1cb2b1841bd932780a65e04c348df6ca15c7bd9a
Beth Hoskins <bhoskins@charter.net>
2015-04-07 11:33:58 -0400

    Initial commit with files, header background and header text styles

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+
+
+
+$(document).ready(function(){
+    //window.router = new AppRouter();
+    //Backbone.history.start();
+    console.log('its working');
+  });
+})();
