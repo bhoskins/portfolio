23328eddfd6ee71b36f667502a2e23a16745f028
Beth Hoskins <bhoskins@charter.net>
2015-04-08 17:43:46 -0400

    styling transition and overlays

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,28 @@ $(document).ready(function(){
     //window.router = new AppRouter();
     //Backbone.history.start();
     console.log('its working');
+    $('.js-toggle-cover').hover(function(){
+      $('.js-food-icon-bw').fadeOut( "fast");
+      $('.food-icon-info').animate(
+        { color: 'blue' }, "fast" );
+    },
+      function(){
+       $('.js-food-icon-bw').fadeIn( "fast" );
+    });
+
+    //slide up animation
+    // $('.js-toggle-cover').on("mouseover", (function(){
+    //   $('.food-icon-info').slideToggle( "slow", function(){
+
+    //   });
+    // }));
+    // $('.js-toggle-cover').on("mouseout", (function(){
+    //   $('.food-icon-info').slideToggle( "slow", function(){
+
+    //   });
+    // }));
+
+
+
   });
 })();
